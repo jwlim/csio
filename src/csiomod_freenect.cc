d4ece149543ba57746807f613d4c86f080950046
Hyon Lim <limhyon@gmail.com>
2014-11-18 02:19:32 +0900

    freenect part modified

--- src/csiomod_freenect.cc
+++ src/csiomod_freenect.cc
@@ -22,24 +22,40 @@
 
 using namespace std;
 
-DEFINE_string(out, "", "File path for csio::OutputStream (- for stdout).");
+DEFINE_string(out, "-", "File path for csio::OutputStream (- for stdout).");
 DEFINE_int32(kinect_devno, 0, "Kinect device number to use.");
 DEFINE_int32(kinect_tilt_angle, 0, "Kinect tilt angle (-30~30 deg.).");
 
-class myMutex {
-  public:
-    myMutex() {
-      pthread_mutex_init( &m_mutex, NULL );
-    }
-    void lock() {
-      pthread_mutex_lock( &m_mutex );
-    }
-    void unlock() {
-      pthread_mutex_unlock( &m_mutex );
-    }
-  private:
-    pthread_mutex_t m_mutex;
+class Mutex {
+public:
+        Mutex() {
+                pthread_mutex_init( &m_mutex, NULL );
+        }
+        void lock() {
+                pthread_mutex_lock( &m_mutex );
+        }
+        void unlock() {
+                pthread_mutex_unlock( &m_mutex );
+        }
+
+        class ScopedLock
+        {
+                Mutex & _mutex;
+        public:
+                ScopedLock(Mutex & mutex)
+                        : _mutex(mutex)
+                {
+                        _mutex.lock();
+                }
+                ~ScopedLock()
+                {
+                        _mutex.unlock();
+                }
+        };
+private:
+        pthread_mutex_t m_mutex;
 };
+
 
 class FreenectWrapper : public Freenect::FreenectDevice {
  public:
@@ -60,19 +76,19 @@ class FreenectWrapper : public Freenect::FreenectDevice {
 
   // Do not call directly even in child
   void VideoCallback(void* _rgb, uint32_t timestamp) {
-    m_rgb_mutex.lock();
+    Mutex::ScopedLock lock(m_rgb_mutex);
+    VLOG(1) << "In video callback";
     rgb = static_cast<uint8_t*>(_rgb);
     m_new_rgb_frame = true;
-    m_rgb_mutex.unlock();
   }
     
   // Do not call directly even in child
   void DepthCallback(void* _depth, uint32_t timestamp) {
-    m_depth_mutex.lock();
+    Mutex::ScopedLock lock(m_depth_mutex);
+    VLOG(1) << "In depth callback";
     depth = static_cast<uint8_t*>(_depth);
     //depth = (uint8_t*)static_cast<uint16_t*>(_depth);
     m_new_depth_frame = true;
-    m_depth_mutex.unlock();
   }
     
   bool getVideo(uint8_t* output) {
@@ -105,10 +121,11 @@ class FreenectWrapper : public Freenect::FreenectDevice {
     std::vector<uint8_t> m_buffer_depth;
     std::vector<uint8_t> m_buffer_rgb;
     std::vector<uint16_t> m_gamma;
-    myMutex m_rgb_mutex;
-    myMutex m_depth_mutex;
+    Mutex m_rgb_mutex;
+    Mutex m_depth_mutex;
     bool m_new_rgb_frame;
     bool m_new_depth_frame;
+
     uint8_t *m_rgb_data;
     uint16_t *m_depth_data; 
 
@@ -122,7 +139,7 @@ int main(int argc, char** argv){
   google::InitGoogleLogging(argv[0]);
 
   Freenect::Freenect freenect;
-  FreenectWrapper& device = freenect.createDevice<FreenectWrapper>(0);
+  FreenectWrapper* device = &freenect.createDevice<FreenectWrapper>(0);
 
   vector<csio::ChannelInfo> channels;
   channels.push_back(csio::ChannelInfo(
@@ -140,9 +157,9 @@ int main(int argc, char** argv){
     return -1;
   }
 
-  device.setDepthFormat(FREENECT_DEPTH_REGISTERED);
-  device.startVideo();
-  device.startDepth();
+  device->setDepthFormat(FREENECT_DEPTH_REGISTERED);
+  device->startVideo();
+  device->startDepth();
 
   bool die(false);
 
@@ -150,14 +167,14 @@ int main(int argc, char** argv){
   uint8_t *depth = new uint8_t[640*480*2];
 
   while(!die) {
-    if (device.getVideo(rgb) && device.getDepth(depth)) {
+    if (device->getVideo(rgb) && device->getDepth(depth)) {
       csio_os.PushSyncMark(2);
       csio_os.Push(0, static_cast<uint8_t*>(rgb), 640 * 480 * 3);
       csio_os.Push(1, static_cast<uint8_t*>(depth), 640 * 480 * 2);
     }
   }
 
-  device.stopVideo();
-  device.stopDepth();
+  device->stopVideo();
+  device->stopDepth();
   return 0;
 }
